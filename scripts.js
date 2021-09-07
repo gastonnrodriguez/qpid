509e87c4885e5a59a7bda3b540237d193a886fc4
gastonnrodriguez <gastonnrodriguez@gmail.com>
2021-09-07 01:00:22 -0300

    primer bosquejo completo

---
+++ scripts.js
@@ -0,0 +1,7 @@
+const menuToggle = document.querySelector('.toggle');
+const video_background = document.querySelector('.video_background');
+
+menuToggle.addEventListener('click', () => {
+  menuToggle.classList.toggle('active');
+  video_background.classList.toggle('active');
+})
