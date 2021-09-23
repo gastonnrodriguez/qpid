f8e699ddb083811dbecfcbdd316fe6e1cc3b6821
gastonnrodriguez <gastonnrodriguez@gmail.com>
2021-09-23 00:45:11 -0300

    cambios en textos, tama;os de fuentes y estructura del sitio

--- scripts.js
+++ scripts.js
@@ -8,3 +8,29 @@ menuBtn.addEventListener("click", () => {
   menuBtn.classList.toggle("active");
   navigation.classList.toggle("active");
 });
+
+//Navbar cambia con scroll
+/*
+window.onscroll = function() {scrollFunction()};
+
+
+function scrollFunction() {
+    let navbar = document.getElementById("navbar")
+  if(navbar !=null){
+    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+        document.getElementById("navbar").classList.remove('theme');    
+        document.getElementById("navbar").classList.add('scrolled');
+        document.getElementById("logo").classList.remove('logo');
+        document.getElementById("logo").classList.add('logo-scrolled');
+        document.querySelector(".selected").classList.add('theme');
+        document.querySelector(".selected").classList.add('text-light');
+      } else{    
+        document.getElementById("navbar").classList.remove('scrolled');
+        document.getElementById("navbar").classList.add('theme');
+        document.getElementById("logo").classList.add('logo');
+        document.getElementById("logo").classList.remove('logo-scrolled'); 
+        document.querySelector(".selected").classList.remove('theme', 'text-light');
+        document.querySelector(".selected").classList.remove('text-light');
+      }
+  }
+}*/
